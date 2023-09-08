9cab983db30da19283286419e5218aa022eabae9
math.μ ✯ <96264224+matthewpenkala@users.noreply.github.com>
2023-09-08 01:46:46 -0600

    Delete @github/hotkey directory

--- @github/hotkey/dist/utils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function isFormField(element: Node): boolean;
-export declare function fireDeterminedAction(el: HTMLElement, path: string[]): void;
-export declare function expandHotkeyToEdges(hotkey: string): string[][];
