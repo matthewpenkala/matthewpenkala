a4f5613c7280196bc84cfda50ef43ab22ef3a9ab
math.μ ✯ <96264224+matthewpenkala@users.noreply.github.com>
2023-09-08 02:34:01 -0600

    Delete @github directory

--- @github/hotkey@2.0.1/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Leaf, RadixTrie } from './radix-trie';
-import eventToHotkeyString from './hotkey';
-export { RadixTrie, Leaf, eventToHotkeyString };
-export declare function install(element: HTMLElement, hotkey?: string): void;
-export declare function uninstall(element: HTMLElement): void;
