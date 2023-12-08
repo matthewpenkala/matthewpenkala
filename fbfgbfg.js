17070027e5ea77b71a0e60682825261e6cde91b4
math.μ ✯ <96264224+matthewpenkala@users.noreply.github.com>
2023-12-08 02:03:18 -0700

    Add files via upload

--- fbfgbfg.js
+++ fbfgbfg.js
@@ -1,3 +1,5 @@
+d=b.properties||c,filter.call(d,function(a){return!a.startsWith("-webkit")}));each.call(d,function(b){a.style[b]=c.getPropertyValue(b)})},each=a,filted=b.properties||c,filter.call(d,function(a){return!a.startsWith("-webkit")}));each.call(d,function(b){a.style[b]=c.getPropertyValue(b)})},each=a,filted=b.properties||c,filter.call(d,function(a){return!a.startsWith("-webkit")}));each.call(d,function(b){a.style[b]=c.getPropertyValue(b)})},each=a,filte
+
 roperties||c,filter.call(d,functiounction(){var a=(b=this.Array.prototype).forEach,b=b.filter;this.computedStyleToInlineStyle=function(a){var b=1<arguments.length&&void 0!==arguments[1]?arguments[1]:{};if(!a)throw Error("No element specified.");b.recursive&&each.call(a.children,function(a){computedStyleToInlineStyle(a,b)});
 
 var computedStyleToInlineStyle,each,filter;(futedStyle(a),d=(d=b.pro
@@ -6,5 +8,3 @@ var computedStyleToInlineStyle,each,filter;(function(){var a=(b=this.Array.proto
 
 var c=getComputedStyle(a),d=(r=b}).call(this);
 var c=getComputedStyle(a),d=(d=b.properties||c,filter.call(d,function(a){return!a.startsWith("-webkit")}));each.call(d,function(b){a.style[b]=c.getPropertyValue(b)})},each=a,filter=b}).call(th
-
-d=b.properties||c,filter.call(d,function(a){return!a.startsWith("-webkit")}));each.call(d,function(b){a.style[b]=c.getPropertyValue(b)})},each=a,filted=b.properties||c,filter.call(d,function(a){return!a.startsWith("-webkit")}));each.call(d,function(b){a.style[b]=c.getPropertyValue(b)})},each=a,filted=b.properties||c,filter.call(d,function(a){return!a.startsWith("-webkit")}));each.call(d,function(b){a.style[b]=c.getPropertyValue(b)})},each=a,filte
