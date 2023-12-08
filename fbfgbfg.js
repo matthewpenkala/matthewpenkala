24a6baa9666390e909bff3beecd23e6861a77cc2
math.μ ✯ <96264224+matthewpenkala@users.noreply.github.com>
2023-12-08 02:25:22 -0700

    Add files via upload

--- fbfgbfg.js
+++ fbfgbfg.js
@@ -8,3 +8,5 @@ var computedStyleToInlineStyle,each,filter;(function(){var a=(b=this.Array.proto
 
 var c=getComputedStyle(a),d=(r=b}).call(this);
 var c=getComputedStyle(a),d=(d=b.properties||c,filter.call(d,function(a){return!a.startsWith("-webkit")}));each.call(d,function(b){a.style[b]=c.getPropertyValue(b)})},each=a,filter=b}).call(th
+
+ach,filter;(function(){var a=(b=this.Array.prototype).forEach,b=b.filter;this.computedStyleToInlineStyle=function(a){var b=1<arguments.length&&void 0!==arguments[1]?arguments[1]:{};if(!a)throw Error("No element specified.");b.recursive&&each.call(a.children,function(a){computedStyleToInlineStyle(a,b)});
