2d4d4b6dee236438b38bcba57cf063aeb6418205
math.μ ✯ <96264224+matthewpenkala@users.noreply.github.com>
2023-12-11 19:03:41 -0700

    Update renderResumePDF.js

--- distJS/renderResumePDF.js
+++ distJS/renderResumePDF.js
@@ -5,7 +5,6 @@
     for (const m of match) {
         assetUrls.push(m[1]);
     }
-
     function preloadAssets(assetUrls, callback) {
         let loadedCount = 0;
         assetUrls.forEach(url => {
@@ -22,50 +21,58 @@
     preloadAssets(assetUrls, function () {
         console.log(assetUrls);
     });
-})();
+}());
 
 var Webflow = Webflow || [];
 Webflow.push(function () {
-    function roundNum(a) {
-        return Math.round(a);
+    function round(number) {
+        return Math.round(number);
     }
 
     const mobileDetect = new MobileDetect(window.navigator.userAgent);
-    if (mobileDetect.mobile() || mobileDetect.phone() || (window.innerWidth <= 478)) {
-        if ((mobileDetect.is('iPhone') || mobileDetect.is('iPod') || mobileDetect.is('iPad')) || (window.innerWidth <= 991)) {
-            $('#OPEN-RESUME-HD, #OPEN-RESUME-FT').attr('href', 'javascript:void(0)').click(function () {
-                const newWindow = window.open('/RESUME.pdf', '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=100');
+
+    if (mobileDetect.mobile() || mobileDetect.phone() || 478 >= window.innerWidth) {
+        if (mobileDetect.is("iPhone") || mobileDetect.is("iPod") || mobileDetect.is("iPad") || 991 >= window.innerWidth) {
+            $("#OPEN-RESUME-HD, #OPEN-RESUME-FT").attr("href", "javascript:void(0)").click(function () {
+                const newWindow = window.open("/RESUME.pdf", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=100");
                 newWindow.focus();
             });
         }
+    } else if (mobileDetect.ua.toLowerCase().includes("applewebkit") && mobileDetect.ua.toLowerCase().includes("safari") && mobileDetect.ua.toLowerCase().includes("macintosh") && !mobileDetect.ua.toLowerCase().includes("chrome")) {
+		$("#OPEN-RESUME-HD, #OPEN-RESUME-FT").find("*").addBack().css("cursor","pointer").filter("a").attr("href", "javascript:void(0)").click(function () {
+            const newWindow = window.open("/RESUME.pdf", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=100");
+            newWindow.focus();
+        });
     } else {
-        const pdfTempWidth = `${roundNum((1.02308 * (parseFloat(17 / 11) * 100)))}vh`;
-        const openResumeHD = document.getElementById('OPEN-RESUME-HD');
-        const openResumeFT = document.getElementById('OPEN-RESUME-FT');
-        const pdfContainer = document.getElementById('pdf-container');
-        const pdfLightbox = document.getElementById('pdf-lightbox');
+        const containerWidth = `${round(1.02308 * (100 * parseFloat(17 / 11)))}vh`;
+        const openResumeHD = document.getElementById("OPEN-RESUME-HD");
+        const openResumeFT = document.getElementById("OPEN-RESUME-FT");
+        const pdfContainer = document.getElementById("pdf-container");
+        const pdfLightbox = document.getElementById("pdf-lightbox");
 
         $(openResumeHD).add($(openResumeFT)).click(function () {
             $("#DOWNLOAD-RESUME").closest("div[class*='wrapper']").css("display", "none").children("div:not(a)").css("opacity", "0%");
-            pdfContainer.innerHTML = '';
-            $(pdfContainer).width(pdfTempWidth).css({
-                'display': 'block',
-                'position': 'absolute',
-                'opacity': '100%'
-            }).height('100vh');
-            pdfjsLib.getDocument('https://cache.matthewpenkala.com/MTP_RESUME_2024.pdf').promise.then(function (pdf) {
-                const renderingPromises = [];
+            pdfContainer.innerHTML = "";
+            $(pdfContainer).width(containerWidth).css({
+                display: "block",
+                position: "absolute",
+                opacity: "100%"
+            }).height("100vh");
 
-                for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
-                    const pageRendering = pdf.getPage(pageNum).then(function (page) {
-                        bodyScrollLock.disableBodyScroll('#pdf-lightbox');
-                        bodyScrollLock.disableBodyScroll('#pdf-container');
+            pdfjsLib.getDocument("https://cache.matthewpenkala.com/MTP_RESUME_2024.pdf").promise.then(function (pdf) {
+                const pagePromises = [];
 
-                        const canvas = document.createElement('canvas');
-                        const context = canvas.getContext('2d');
+                for (let pageNumber = 1; pageNumber <= pdf.numPages; pageNumber++) {
+                    const pagePromise = pdf.getPage(pageNumber).then(function (page) {
+                        bodyScrollLock.disableBodyScroll("#pdf-lightbox");
+                        bodyScrollLock.disableBodyScroll("#pdf-container");
+
+                        const canvas = document.createElement("canvas");
+                        const context = canvas.getContext("2d");
                         const viewport = page.getViewport({
                             scale: 1
                         });
+
                         canvas.width = viewport.width;
                         canvas.height = viewport.height;
                         pdfContainer.appendChild(canvas);
@@ -76,31 +83,33 @@ Webflow.push(function () {
                         }).promise;
                     });
 
-                    renderingPromises.push(pageRendering);
+                    pagePromises.push(pagePromise);
                 }
 
-                Promise.all(renderingPromises).then(function () {
-                    $('#DOWNLOAD-RESUME').closest("div[class*='wrapper']").css('display', '').children('div:not(a)').fadeOut('fast', function () {
-                        $(this).css('opacity', '').fadeIn().css('opacity', '');
+                Promise.all(pagePromises).then(function () {
+                    $("#DOWNLOAD-RESUME").closest("div[class*='wrapper']").css("display", "").children("div:not(a)").fadeOut("fast", function () {
+                        $(this).css("opacity", "").fadeIn().css("opacity", "");
                     });
+
                     $(pdfContainer).css({
-                        'width': '',
-                        'display': '',
-                        'position': '',
-                        'opacity': '',
-                        'height': '',
+                        width: "",
+                        display: "",
+                        position: "",
+                        opacity: "",
+                        height: ""
                     });
-                    $('div.certifications').closest("[class*='wrapper']").fadeOut();
-                    pdfLightbox.style.display = 'flex';
-                }).catch(function (error) {
-                    window.open('/RESUME.pdf', '_blank').focus();
+
+                    $("div.certifications").closest("[class*='wrapper']").fadeOut();
+                    pdfLightbox.style.display = "flex";
+                }).catch(function () {
+                    window.open("/RESUME.pdf", "_blank").focus();
                 });
-            }).catch(function (error) {
-                window.open('/RESUME.pdf', '_blank').focus();
+            }).catch(function () {
+                window.open("/RESUME.pdf", "_blank").focus();
             });
 
-            pdfLightbox.style.display = 'flex';
-            $('div.certifications').closest("[class*='wrapper']").fadeOut();
+            pdfLightbox.style.display = "flex";
+            $("div.certifications").closest("[class*='wrapper']").fadeOut();
         });
     }
 });
